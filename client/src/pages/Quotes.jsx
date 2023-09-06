6700b581f756710934540f21b38a059b8d26cfb4
michellejanay <mindyourphotosynthesis@gmail.com>
2023-09-06 17:14:51 +0100

    fix: loading spinner added

--- client/src/pages/Quotes.jsx
+++ client/src/pages/Quotes.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import Quote from '../components/Quote'
+import React from "react";
+import { useState, useEffect } from "react";
+import Quote from "../components/Quote";
+import { PropagateLoader } from "react-spinners";
 
 const Quotes = () => {
-
-  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('https://michellejanay-quote-server.glitch.me/quotes')
+    fetch("https://michellejanay-quote-server.glitch.me/quotes")
       .then((response) => response.json())
       .then((data) => setData(data))
-  }, [])
+      .then(() => setLoading(false));
+  }, []);
   return (
     <div>
-      <header className="quotes-header">
-      </header>
-      {data.map((q) => (
-        <Quote quote={q.quote} author={q.author} key={data.indexOf(q)} />
-      ))}
+      <header className="quotes-header"></header>
+      {loading && <PropagateLoader color="rgba(0, 0, 0, 1)" />}
+      {data &&
+        data.map((q) => (
+          <Quote quote={q.quote} author={q.author} key={data.indexOf(q)} />
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Quotes
+export default Quotes;
